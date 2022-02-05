c98aa9a406896830410799a226fb3addeffcee08
John Pol <shadowinhaze@gmail.com>
2022-02-05 22:55:53 +0100

    Add some utils
    * fix: change prettier config;
    feat: add api-get-words method (without authorization);
    feat: add global types in typings.d.ts;
    feat: add some utils funcs (shuffle and random num);
    
    * refactor: add error catching for API method;

---
+++ src/api/get-words.tsx
@@ -0,0 +1,18 @@
+import { base } from './api';
+// const local = 'http://localhost:3007';
+
+const GetWords = async (group = 0, page = 0) => {
+  try {
+    const rawResponse = await fetch(
+      `${base}/words?group=${group}&page=${page}`
+    );
+    const words = await rawResponse.json();
+    if (words.length === 0)
+      throw new Error('Check your request >>>>> Beyond the textbook');
+    return words;
+  } catch (error) {
+    if (error instanceof Error) console.error('Error caught >>>>> ' + error);
+  }
+};
+
+export { GetWords };

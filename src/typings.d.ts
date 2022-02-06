f7b1c1cc57a910e31f92a9ef17b8a43e28f31384
BlackBerryID <iddf1994one@gmail.com>
2022-02-06 12:29:12 +0300

    refactor: put all types in the same file

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -14,3 +14,52 @@ type Word = {
   textMeaningTranslate: string;
   textExampleTranslate: string;
 };
+
+type NavLinkProps = {
+  path: string;
+  text: string;
+  size: MUIVariant;
+};
+
+type SideMenuItemProps = {
+  path: string;
+  text: string;
+  toggleSideMenu: ToggleSideMenuFunc;
+};
+
+type SideMenuProps = {
+  isMenuOpen: boolean;
+  toggleSideMenu: ToggleSideMenuFunc;
+};
+
+type ToggleSideMenuFunc = (
+  open: boolean
+) => (event: React.KeyboardEvent | React.MouseEvent) => void;
+
+type NavGamesProps = {
+  toggleSideMenu: ToggleSideMenuFunc;
+};
+
+type MUIVariant =
+  | 'button'
+  | 'caption'
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'inherit'
+  | 'subtitle1'
+  | 'subtitle2'
+  | 'body1'
+  | 'body2'
+  | 'overline'
+  | undefined;
+
+type SideMenuProps = {
+  isMenuOpen: boolean;
+  toggleSideMenu: (
+    open: boolean
+  ) => (event: React.KeyboardEvent | React.MouseEvent) => void;
+};

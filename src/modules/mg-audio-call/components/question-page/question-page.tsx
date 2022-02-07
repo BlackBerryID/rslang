533cee9ee833fc4f1652b4e0b42fd644605d3e4f
shadowinhaze <shadowinhaze@gmail.com>
2022-02-07 15:51:17 +0100

    refactor: add finally to tru/catch

--- src/modules/mg-audio-call/components/question-page/question-page.tsx
+++ src/modules/mg-audio-call/components/question-page/question-page.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { CircularProgress, IconButton } from "@mui/material";
-import { GetWords } from "../../../../api/get-words";
-import { GetRandomNum } from "../../../../utils/get-random-num";
+import React, { useEffect, useState } from 'react';
+import { CircularProgress, IconButton } from '@mui/material';
+import { GetWords } from '../../../../api/get-words';
+import { GetRandomNum } from '../../../../utils/get-random-num';
 
 import './question-page.scss';
-import Answers from "../question/answers";
-import { VolumeUp } from "@mui/icons-material";
+import Answers from '../question/answers';
+import { VolumeUp } from '@mui/icons-material';
 
 const QuestionPage = ({ difficulty }: { difficulty: number }) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -16,21 +16,21 @@ const QuestionPage = ({ difficulty }: { difficulty: number }) => {
     try {
       GetWords(difficulty, GetRandomNum(0, 29)).then((words) => {
         setWords(words);
-        setIsLoading(false);
       });
     } catch (error) {
       // TODO: add Error Boundary (@saratovkin)
       console.log(error);
+    } finally {
       setIsLoading(false);
     }
   }, [difficulty]);
 
-  // TODO: get 5 random unique words (@saratovkin) 
-  const displayedWords = words.slice(0, 5).map((item: Word) => item.wordTranslate);
+  // TODO: get 5 random unique words (@saratovkin)
+  const displayedWords = words
+    .slice(0, 5)
+    .map((item: Word) => item.wordTranslate);
   if (isLoading) {
-    return (
-      <CircularProgress />
-    );
+    return <CircularProgress />;
   } else {
     return (
       <div className="question-page">

72f89a87cc5353b588c46637da4c0b0f611e33e0
shadowinhaze <shadowinhaze@gmail.com>
2022-02-07 14:09:08 +0100

    refacotr: change audio-call spelling, remove extra classes, becauseof base container

---
+++ src/modules/mg-audio-call/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StartPage from './components/start-page';
+
+import './audio-call.scss';
+
+export const MiniGameAudioCall = () => {
+  return (
+    <div className="audio-call">
+      <StartPage />
+    </div>
+  );
+};

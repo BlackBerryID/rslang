1bd6ced911eaa99c7aa516754374c1b8f3d8914f
@shadowinhaze <shadowinhaze@gmail.com>
2022-02-10 18:05:31 +0100

    feat: add global redux-store implementation

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -15,6 +15,14 @@ type Word = {
   textExampleTranslate: string;
 };
 
+type User = {
+  message: string;
+  token: string;
+  refreshToken: string;
+  userId: string;
+  name: string;
+};
+
 type NavLinkProps = {
   path: string;
   text: string;

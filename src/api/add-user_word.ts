47f2eefaa68988d854e4c2d676eee4899929a547
@shadowinhaze <shadowinhaze@gmail.com>
2022-02-13 02:50:06 +0100

    feat: add create UW api method

--- src/api/add-user_word.ts
+++ src/api/add-user_word.ts
@@ -1,40 +1,42 @@
 import { base } from '.';
-import { GetUserWord } from './get-user_word';
 
-export const UpdateUserWord = async ({
+export const AddUserWord = async ({
   userId,
   userToken,
   wordId,
   updateReq,
 }: AdditionUserWord) => {
   const url = `${base}/users/${userId}/words/${wordId}`;
-  try {
-    const currentWordState = await GetUserWord({ userId, userToken, wordId });
-    const patch = {
-      difficulty: currentWordState.difficulty,
-      optional: currentWordState.optional,
+  const defCreqationReq = {
+    difficulty: 'learning',
+    optional: {
+      audioStreak: ' ',
+      sprintStreak: ' ',
+    },
+  };
+
+  if (updateReq.difficulty) {
+    defCreqationReq.difficulty = updateReq.difficulty;
+  }
+
+  if (updateReq.optional) {
+    const patchOptional = {
+      ...defCreqationReq.optional,
+      ...updateReq.optional,
     };
+    defCreqationReq.optional = patchOptional;
+  }
 
-    if (updateReq.difficulty) {
-      patch.difficulty = updateReq.difficulty;
-    }
-
-    if (updateReq.optional) {
-      const patchOptional = {
-        ...currentWordState.optional,
-        ...updateReq.optional,
-      };
-      patch.optional = patchOptional;
-    }
-
+  console.log(defCreqationReq);
+  try {
     const rawResponse = await fetch(url, {
-      method: 'PUT',
+      method: 'POST',
       headers: {
         Authorization: `Bearer ${userToken}`,
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(patch),
+      body: JSON.stringify(defCreqationReq),
     });
 
     switch (rawResponse.status) {
@@ -48,6 +50,8 @@ export const UpdateUserWord = async ({
         );
       case 404:
         throw new Error(`Word with this id not found in user's colleciton.`);
+      case 417:
+        throw new Error(`Sorry, but such user word already exists.`);
     }
   } catch (err) {
     if (err instanceof Error)

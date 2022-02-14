20b768e294b9ce8d85746217618cc7f08579dce1
BlackBerryID <iddf1994one@gmail.com>
2022-02-14 16:54:54 +0300

    feat: pagination work

--- src/modules/textbook/index.tsx
+++ src/modules/textbook/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { Container, Box, Pagination } from '@mui/material';
 
 import './textbook.scss';
@@ -7,9 +7,34 @@ import { TextbookLevels } from './components/textbook-levels';
 import { TextbookWords } from './containers/textbook-words';
 import { TextbookCard } from './components/textbook-card';
 import { TextbookGames } from './components/textbook-games';
+import { GetWords } from '../../api';
 
 export const Textbook = () => {
   const [currentColor, setCurrentColor] = useState('#fdd835');
+  const [group, setGroup] = useState(
+    localStorage.getItem('group')
+      ? JSON.parse(localStorage.getItem('group') as string)
+      : 0
+  );
+  const [page, setPage] = useState(
+    localStorage.getItem('page')
+      ? JSON.parse(localStorage.getItem('page') as string)
+      : 0
+  );
+  const [words, setWords] = useState(null);
+
+  const getWords = useCallback(async () => {
+    let response = await GetWords(group, page);
+    setWords(response);
+  }, [group, page]);
+
+  useEffect(() => {
+    getWords();
+  }, [getWords]);
+
+  const changePage = (e: React.ChangeEvent<unknown>, page: number) => {
+    setPage(page - 1);
+  };
 
   return (
     <Container className="textbook_container">
@@ -18,8 +43,13 @@ export const Textbook = () => {
       <div className="textbook_words__title">Слова</div>
       <Box className="textbook_main" sx={{ pt: '20px' }}>
         <Box className="textbook_main__left">
-          <TextbookWords color={currentColor} />
-          <Pagination count={10} color="primary" sx={{ mt: '30px' }} />
+          <TextbookWords color={currentColor} words={words} />
+          <Pagination
+            count={30}
+            color="primary"
+            sx={{ mt: '30px' }}
+            onChange={changePage}
+          />
           <TextbookGames color={currentColor} />
         </Box>
         <TextbookCard />
--- src/modules/textbook/textbook.d.ts
+++ src/modules/textbook/textbook.d.ts
@@ -8,3 +8,25 @@ type TextbookLevelsProps = {
 };
 
 declare module '*.jpg';
+
+type TextbookWordsProps = {
+  color: string;
+  words: Array<GetWord> | null;
+};
+
+type GetWord = {
+  id: string;
+  group: number;
+  page: number;
+  word: string;
+  image: string;
+  audio: string;
+  audioMeaning: string;
+  audioExample: string;
+  textMeaning: string;
+  textExample: string;
+  transcription: string;
+  wordTranslate: string;
+  textMeaningTranslate: string;
+  textExampleTranslate: string;
+};

63c801bafcf04528ed951509962564edf6910e17
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2022-02-17 01:23:00 +0300

    feat: connect api methods to statistics page

--- src/modules/statistics/components/long-stats/index.tsx
+++ src/modules/statistics/components/long-stats/index.tsx
@@ -1,9 +1,11 @@
-import { Container, Typography } from "@mui/material";
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
+import { Container, Grid, Switch, Typography } from "@mui/material";
 import {
   ResponsiveContainer,
   LineChart,
+  BarChart,
   Line,
+  Bar,
   XAxis,
   YAxis,
   CartesianGrid,
@@ -14,33 +16,67 @@ import './long-stats.scss';
 
 // TODO: add correct type of data @saratovkin
 const LongStats = ({ data }: { data: any }) => {
+  const [graphType, setGraphType] = useState(true);
+
   return (
     <Container sx={{ textAlign: "center", mt: 10, mb: 5 }}>
       <Typography variant="h2" sx={{ mb: 5 }} >Статистика за всё время</Typography>
-      <ResponsiveContainer width="95%" height={400} >
-        <LineChart
-          width={1000}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Line
-            type="monotone"
-            dataKey="words"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
+      <Grid container alignItems="center" justifyContent="center" sx={{ mb: 5 }}>
+        <Grid item>Количество слов</Grid>
+        <Grid item>
+          <Switch
+            onChange={() => setGraphType(type => !type)}
+            color="default"
           />
-        </LineChart>
-      </ResponsiveContainer>
+        </Grid>
+        <Grid item>Прогресс</Grid>
+      </Grid>
+      {graphType ?
+        <ResponsiveContainer width="95%" height={400} >
+          <LineChart
+            width={1000}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Line
+              type="monotone"
+              dataKey="totalLearned"
+              stroke="#8ca9d3"
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
+        </ResponsiveContainer> :
+        <ResponsiveContainer width="95%" height={400} >
+
+          <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="learnedToday" fill="#8ca9d3" />
+          </BarChart>
+        </ResponsiveContainer>
+      }
     </Container>
   );
 };
--- src/modules/statistics/index.tsx
+++ src/modules/statistics/index.tsx
@@ -1,34 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { Container } from "@mui/material";
 import { LongStats } from "./components/long-stats";
 import { TodayStats } from "./components/today-stats";
 import { GuestAlert } from "./components/guest-alert";
-
-const isLoggedIn = false;
+import { RootState } from "../../store";
+import { GetUserStats } from "../../api/get-user_stats";
+import { UpdateUserStats } from "../../api/update-user_stats";
 
 const globalStats = [25, 50];
 const sprintStats = { wordsAmount: 5, percentage: 35, streak: 2 };
 const audiocallStats = { wordsAmount: 2, percentage: 10, streak: 1 };
 
-// TODO: add correct structure of this array @saratovkin
-const longStats = [{
-  date: "0",
-  words: 1,
-}, {
-  date: "1",
-  words: 5,
-}, {
-  date: "7",
-  words: 3,
-}, {
-  date: "12",
-  words: 5,
-}, {
-  date: "13",
-  words: 25,
-}];
-
 const Statistics = () => {
+
+  const [stats, setStats] = useState<any[]>();
+  const { userId, token } = useSelector((state: RootState) => state.user);
+
+  useEffect(() => {
+    if (userId) {
+      GetUserStats(userId, token).then((response: UserStats) => {
+        if (response.optional) {
+          if (response.optional.stats) {
+            setStats(Object.values(response.optional.stats));
+          }
+        }
+      }).catch((error) => console.log(error));
+    }
+  }, [userId, token]);
 
   return (
     <Container>
@@ -37,7 +36,7 @@ const Statistics = () => {
         sprintStats={sprintStats}
         audiocallStats={audiocallStats}
       />
-      {isLoggedIn ? <LongStats data={longStats} /> : <GuestAlert />}
+      {userId.length ? <LongStats data={stats} /> : <GuestAlert />}
     </Container>
   );
 };

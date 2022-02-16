1a76f4706fb483d210e283fae356b847df5a098d
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2022-02-17 01:24:05 +0300

    feat: add helper function

---
+++ src/utils/format-date.ts
@@ -0,0 +1,6 @@
+export function formatDate(date: Date): string {
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = String(date.getFullYear()).slice(-2);
+  return `${day}.${month}.${year}`;
+}

0179d0602a19d721ab6d3bfbf06b9ce6eccbc36b
@shadowinhaze <shadowinhaze@gmail.com>
2022-02-18 09:51:39 +0100

    refactor: version propolsal for utils method

--- src/utils/check-is-learned.ts
+++ src/utils/check-is-learned.ts
@@ -1,3 +1,5 @@
+import { AnswersToChangeStatus } from '../app/constants';
+
 const IS_LEARNED_REGULAR = 3;
 const IS_LEARNED_DIFFICULT = 5;
 
@@ -5,13 +7,28 @@ export function checkIsLearned(answers: string, status: string): boolean {
   if (status === 'learned') {
     return false;
   }
-  const count = status === 'learning' ? IS_LEARNED_REGULAR : IS_LEARNED_DIFFICULT;
+  const count =
+    status === 'learning' ? IS_LEARNED_REGULAR : IS_LEARNED_DIFFICULT;
   if (answers.length < count) {
     return false;
   }
-  const checkSum = +answers.slice(-count).split('').reduce((a, b) => String(Number.parseInt(a) + Number.parseInt(b)));
+  const checkSum = +answers
+    .slice(-count)
+    .split('')
+    .reduce((a, b) => String(Number.parseInt(a) + Number.parseInt(b)));
   if (checkSum === count) {
     return true;
   }
   return false;
-};
+}
+
+export function checkIsLearnedRed(answers: string, mode: string): boolean {
+  const slice =
+    mode === 'hard' ? AnswersToChangeStatus.hard : AnswersToChangeStatus.simple;
+  return (
+    answers
+      .split('')
+      .slice(-slice)
+      .reduce((acc, game) => acc + +game, 0) === slice
+  );
+}

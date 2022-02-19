c555e599e9e8ba28075a0be7c956e1679f0f1823
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2022-02-19 15:08:22 +0300

    refactor: refactor utils func

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,6 +1,3 @@
-export function formatDate(date: Date): string {
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = String(date.getFullYear()).slice(-2);
-  return `${day}.${month}.${year}`;
+export function formatDate(): string {
+  return new Date().toISOString().split('T')[0].split('-').reverse().join('.');
 }

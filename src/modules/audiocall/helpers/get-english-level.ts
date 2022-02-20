3f043aceb68938ec76186978f8d9e709450cea93
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2022-02-20 23:32:51 +0300

    refactor: update helper func

--- src/modules/audiocall/helpers/get-english-level.ts
+++ src/modules/audiocall/helpers/get-english-level.ts
@@ -1,6 +1,6 @@
 const getLevel = (idx: number): string => {
-  const codeCof = idx > 3 ? 2 : (idx > 1 ? 1 : 0)
-  return `${String.fromCharCode(65 + codeCof)}${idx % 2 + 1}`
+  const codeCof = Math.floor(idx / 2);
+  return `${String.fromCharCode(65 + codeCof)}${idx % 2 + 1}`;
 };
 
 export { getLevel };
